c0148766ce47210ea7a86ea2d0bf7af1d21502e2
duan_junyu.hoperun <duanjunyu@163.com>
2013-06-27 11:13:53 +0800

    modify noreturn.c

--- c_source/noreturn.c
+++ c_source/noreturn.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-int foo(int x);
+int foo(int x, int y);
 int main()
 {
     int i = 7;
-    int ret = foo(i);
+    int ret = foo(i, 0);
     printf("ret = %d\n", ret);
 }
-int foo(int x)
+int foo(int x, int y)
 {
 #if 0
     if(x == 2)
